1fadbc93380b7b76ef01fb9b24ab0089b65694aa
Zandile <zandileignz472@gmail.com>
2023-10-30 23:43:11 +0200

    Prototype classes + main
    added the prototype methods of cloning the waiter

---
+++ Waiter.h
@@ -0,0 +1,31 @@
+#ifndef WAITER_H
+#define WAITER_H
+
+#include <string>
+#include "Order.h"
+
+class WaiterPrototype {
+public:
+    virtual WaiterPrototype* clone() = 0;
+    virtual void takeOrder(int tableNumber, const std::string& dish, Order& order) = 0;
+    virtual void serveOrders(const Order& order) = 0;
+    virtual void setID(int id) = 0;
+    virtual int getID() const = 0;
+    virtual ~WaiterPrototype() {}
+};
+
+class Waiter : public WaiterPrototype {
+public:
+    Waiter();
+    WaiterPrototype* clone() override;
+    void takeOrder(int tableNumber, const std::string& dish, Order& order) override;
+    void serveOrders(const Order& order) override;
+    void setID(int id);
+    int getID() const;
+
+private:
+    int id;
+    // Additional attributes and methods as needed
+};
+
+#endif // WAITER_H
---
+++ amain.cpp
@@ -0,0 +1,24 @@
+#include "Waiter.h"
+#include <iostream>
+
+int main() {
+    // Create an initial waiter
+    WaiterPrototype* originalWaiter = new Waiter();
+    originalWaiter->setID(1);
+
+    // Clone the waiter
+    WaiterPrototype* clonedWaiter = originalWaiter->clone();
+
+    // Check if the cloned waiter is different
+    if (originalWaiter->getID() != clonedWaiter->getID()) {
+        std::cout << "Waiter objects are different.\n";
+    } else {
+        std::cout << "Waiter objects are the same.\n";
+    }
+
+    // Clean up memory
+    delete originalWaiter;
+    delete clonedWaiter;
+
+    return 0;
+}
